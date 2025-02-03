61fe7d567b13e632c8eb673db46ff0cec702a5b7
Nikita Gorynin <nikita12341@mail.ru>
2025-02-03 18:51:01 +0300

    add .env

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
 start:
 	uv run python3 manage.py runserver
+
+shell:
+	uv run python3 manage.py shell
--- hexlet_django_blog/settings.py
+++ hexlet_django_blog/settings.py
@@ -9,8 +9,12 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +24,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ksrz8_clnbv^4=bq+l9^o5n-$ru%7n@@gtrw7_!)(55w7pf#8n'
+# SECRET_KEY = 'django-insecure-ksrz8_clnbv^4=bq+l9^o5n-$ru%7n@@gtrw7_!)(55w7pf#8n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+
+SECRET_KEY = os.getenv('SECRET_KEY')
+DEBUG = os.getenv('DEBUG', False)
 
 ALLOWED_HOSTS = []
 
