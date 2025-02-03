a05d3b1ab729a65502a4d97ac56f589319a85ff8
Nikita Gorynin <nikita12341@mail.ru>
2025-02-03 13:43:02 +0300

    add Article model

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from hexlet_django_blog.article import views
+# from hexlet_django_blog.article import views
+from .views import ArticleIndexView
 
 
 urlpatterns = [
-    path('', views.index),
+    path('', ArticleIndexView.as_view(), name='article_index')
 ]
--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,9 +1,16 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+# from django.shortcuts import render
+# from django.http import HttpResponse
 # Create your views here.
+from django.views import View
+from django.shortcuts import render
 
 
-def index(request):
-    return render(request, 'articles/index.html', context={
-        'app_name': 'hexlet_django_blog.article'
-    })
+# def index(request):
+#  return render(request, 'articles/index.html', context={
+#         'app_name': 'hexlet_django_blog.article'
+#     })
+
+class ArticleIndexView(View):
+    def get(self, request, *args, **kwargs):
+        context = {'app_name': 'hexlet_django_blog.article'}
+        return render(request, 'articles/index.html', context)
