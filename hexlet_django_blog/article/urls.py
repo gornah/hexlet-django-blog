416bdb9c3ae47e3f1514673c531d3c1214851226
Nikita Gorynin <nikita12341@mail.ru>
2025-02-07 14:14:09 +0300

    add delete

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -6,7 +6,8 @@ from hexlet_django_blog.article.views import (
     IndexView,
     ArticleView,
     ArticleFormCreateView,
-    ArticleFormEditView
+    ArticleFormEditView,
+    ArticleFormDeleteView
 )
 
 
@@ -15,5 +16,6 @@ urlpatterns = [
     path('<int:id>/edit/', ArticleFormEditView.as_view(), name='articles_update'),
     path('<int:id>', ArticleView.as_view(), name='article_detail'),
     path('create/', ArticleFormCreateView.as_view(), name='articles_create'),
+    path('<int:id>/delete/', ArticleFormDeleteView.as_view(), name='articles_delete'),
     # path('<inr:article_id>/comment/<int:id>', ArticleCommentsView.as_view()),
 ]
